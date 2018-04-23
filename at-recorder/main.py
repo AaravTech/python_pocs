2f2ee591a2ead1994a84ff2664fc65221bff6563
hardik-p <hardik-p@hardik.p>
2018-04-23 10:23:29 +0530

    Recorder is updated

--- at-recorder/main.py
+++ at-recorder/main.py
@@ -1,59 +1,61 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
 from capture import CaptureScreen
 from datetime import datetime
 
-class Application(tk.Frame):
+class Application(Frame):
     def __init__(self, isapp=True, name="at_recorder"):
-        tk.Frame.__init__(self, name=name)
-        self.grid()
-        self.master.minsize(100, 100)
+        Frame.__init__(self, name=name)
+        # self.master.minsize(100, 100)
         self.master.title('AT Recorder')
         self.isapp = isapp
         self._generate_menu()
-        self._create_widgets()
+        self._create_widgets()        
+        self.grid()
 
     def _create_widgets(self):
-        self.start_recording_btn = tk.Button(self,
+        self.path_lbl = Label(self)
+        self.path_lbl.grid(row=0, columnspan=3, column=0)
+        self.browse_btn = Button(self, text="Browse", command=self.browsefunc)
+        self.browse_btn.grid(row=0, column=3)
+
+        o_r = 1
+        self.start_recording_btn = Button(self,
                                             text="Start",
                                             bg="green",
                                             fg="white",
                                             command=self.start_recording)
-        self.start_recording_btn.grid()
+        self.start_recording_btn.grid(row=o_r, column=0)
         self.bind_all('<Control-s>', self.start_recording)
 
-        self.pause_recording_btn = tk.Button(self,
+        self.pause_recording_btn = Button(self,
                                             text="Pause",
                                             bg="orange",
                                             fg="white",
                                             state="disabled",
                                             command=self.pause_recording)
-        self.pause_recording_btn.grid()
+        self.pause_recording_btn.grid(row=o_r, column=1)
         self.bind_all('<Control-p>', self.pause_recording)
 
-        self.resume_recording_btn = tk.Button(self,
+        self.resume_recording_btn = Button(self,
                                             text="Resume",
                                             bg="blue",
                                             fg="white",
                                             state="disabled",
                                             command=self.resume_recording)
-        self.resume_recording_btn.grid()
+        self.resume_recording_btn.grid(row=o_r, column=2)
         self.bind_all('<Control-r>', self.resume_recording)
 
-        self.stop_recording_btn = tk.Button(self,
+        self.stop_recording_btn = Button(self,
                                             text="Stop",
                                             bg="red",
                                             fg="white",
                                             state="disabled",
                                             command=self.stop_recording)
 
-        self.stop_recording_btn.grid()
+        self.stop_recording_btn.grid(row=o_r, column=3)
         self.bind_all('<Control-x>', self.stop_recording)
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.grid()
-
-        self.bind_all('<Control-c>', self.master.destroy)
+        self.bind_all('<Control-c>', self.close_app)
 
     def start_recording(self, event=None):
         self.master.iconify()
@@ -82,19 +84,26 @@ class Application(tk.Frame):
         self.resume_recording_btn['state'] = 'disabled'
         self.recorder.stop()
 
+    def close_app(self, event=None):
+        self.master.destroy()
+
+    def browsefunc(self, event=None):
+        self.directory = filedialog.askdirectory()
+        self.path_lbl.config(text=self.directory)
+
     def key(self, event):
         print("pressed", repr(event.char))
 
 
     def _generate_menu(self):
-        menubar = tk.Menu(self.master)
+        menubar = Menu(self.master)
 
         # create a pulldown menu, and add it to the menu bar
-        filemenu = tk.Menu(menubar, tearoff=0)
+        filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="Exit", command=self.master.destroy)
         menubar.add_cascade(label="File", menu=filemenu)
 
-        helpmenu = tk.Menu(menubar, tearoff=0)
+        helpmenu = Menu(menubar, tearoff=0)
         menubar.add_cascade(label="Help", menu=helpmenu)
 
         # display the menu
